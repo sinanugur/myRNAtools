1bbedfd6dd44a81ec2345c92e93adad1d09cafdb
Sinan Ugur Umu <sinan.ugur.umu@kreftregisteret.no>
2019-05-10 11:13:05 +0200

    the new variant calling script with parallel programming

--- bin/extract_from_FASTA.py
+++ bin/extract_from_FASTA.py
@@ -2,13 +2,10 @@
 
 '''
 Created on 14/05/2013
-Update on 01/02/2019
+Update on 11/03/2019
 
 @author: suu13
 '''
-from __future__ import print_function
-
-
 
 __licence__="""
 MIT License
@@ -59,8 +56,6 @@ Options:
 
 """
 
-
-
 #prevent sigpipe error
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL)
@@ -73,50 +68,72 @@ import gzip
 import sys
 
 
-def function_read_text_file(txt_file):
-    
-    if txt_file == None:
-        txt_lines = sys.stdin.readlines()
-    else:
-        with open(txt_file) as txt:
-            txt_lines = txt.readlines()
-    ids = list(filter(None, list(map(lambda x: x.strip(), txt_lines))))  # filter endlines and empty items from the text file
-    return ids
+class my_functions:
+    #last update 28/02/2019
 
-def function_parse_sequence_file(fasta_file):
-    file_sequences_dictionary={}
-    if fasta_file.lower().endswith(".gz"):
-        with gzip.open(fasta_file,"rt") as handle:
-            
-            if True:
-                file_sequences = SeqIO.parse(handle, "fastq")
-            else:
-                file_sequences = SeqIO.parse(handle, "fasta")
+    #this is a stdin or text file reader, it takes a single argument which is the text file name.
+    #If that file is not available, then reads STDIN
+    #Requires
+    #sys
+    @staticmethod
+    def function_read_text_file_or_stdin(txt_file):
+        if txt_file == None:
+            txt_lines = sys.stdin.readlines()
+        else:
+            with open(txt_file) as txt:
+                txt_lines = txt.readlines()
+        lines = list(filter(None, list(
+            map(lambda x: x.strip(), txt_lines))))  # filter endlines and empty items from the text file
+        return lines
 
-            for i in file_sequences:
-                file_sequences_dictionary[i.id] = i
-    else:
-        with open(fasta_file,"r") as handle:
-            if True:
-                file_sequences = SeqIO.parse(fasta_file, "fastq")
-            else:
-                file_sequences = SeqIO.parse(fasta_file, "fasta")
-            
-            for i in file_sequences:
-                file_sequences_dictionary[i.id] = i
 
-    return file_sequences_dictionary
+    #fasta or fastq parser
+    #can detect .gz
+    #reads into memory
+    #return to a dictionary
+    @staticmethod
+    def function_parse_sequence_file(fasta_file):
+        file_sequences_dictionary = {}
+        if fasta_file.lower().endswith(".gz"):
+            with gzip.open(fasta_file, "rt") as handle:
+                first_character=handle.read(1)
+
+                if first_character == "@":
+                    handle.seek(0)
+                    file_sequences = SeqIO.parse(handle, "fastq")
+                else:
+                    handle.seek(0)
+                    file_sequences = SeqIO.parse(handle, "fasta")
+
+                for i in file_sequences:
+                    file_sequences_dictionary[i.id] = i
+
+        else:
+            with open(fasta_file, "r") as handle:
+                first_character = handle.read(1)
+
+                if first_character == "@":
+                    handle.seek(0)
+                    file_sequences = SeqIO.parse(handle, "fastq")
+                else:
+                    handle.seek(0)
+                    file_sequences = SeqIO.parse(handle, "fasta")
+
+                for i in file_sequences:
+                    file_sequences_dictionary[i.id] = i
+
+        return file_sequences_dictionary
+
 
 def function_print_output(items_to_print):
     for item in items_to_print:
-        #print (">%s\n%s" % (item.id,item.seq))
-        SeqIO.write(item,sys.stdout,"fastq")
+        SeqIO.write(item,sys.stdout,"fasta")
 
 def keyword_finder_from_fasta_headers_nonexact(txt_file,fasta_file): #function to find nonexact match
 
-    ids=function_read_text_file(txt_file)
-    file_sequences_dictionary=function_parse_sequence_file(fasta_file)
-    if arguments['--reverse']==False: #print the intersection
+    ids = my_functions.function_read_text_file_or_stdin(txt_file)
+    file_sequences_dictionary = my_functions.function_parse_sequence_file(fasta_file)
+    if arguments['--reverse'] == False: #print the intersection
         union=list(filter(lambda x: reduce(lambda a,b: a or b,list(map(lambda y: x.find(y) >= 0,ids))),file_sequences_dictionary.keys()))
         items_to_print=map(lambda x: file_sequences_dictionary.pop(x), union) #pop out the items
         function_print_output(items_to_print)
@@ -128,22 +145,24 @@ def keyword_finder_from_fasta_headers_nonexact(txt_file,fasta_file): #function t
 
 def keyword_finder_from_fasta_headers(txt_file,fasta_file): #exact match
 
-    ids = function_read_text_file(txt_file)
-    file_sequences_dictionary=function_parse_sequence_file(fasta_file)
+    ids = my_functions.function_read_text_file_or_stdin(txt_file)
+    file_sequences_dictionary=my_functions.function_parse_sequence_file(fasta_file)
     if arguments['--reverse']==False: #print the intersection
-        #union=list(filter(lambda x:x in ids,file_sequences_dictionary.keys()))
-        #items_to_print=map(lambda x: file_sequences_dictionary.pop(x), union) #pop out the items
-        #function_print_output(items_to_print)
         for item in ids:
             try:
                 SeqIO.write(file_sequences_dictionary[item],sys.stdout,"fastq")
             except:
-                pass
+                SeqIO.write(file_sequences_dictionary[item], sys.stdout, "fasta")
 
     else: #print the non-intersection disjoint
         disjoint=list(filter(lambda x:x not in ids,file_sequences_dictionary.keys()))
-        items_to_print=map(lambda x: file_sequences_dictionary.pop(x), disjoint)
-        function_print_output(items_to_print)
+        items_to_print=list(map(lambda x: file_sequences_dictionary.pop(x), disjoint))
+        #print(items_to_print)
+        for item in items_to_print:
+            try:
+                SeqIO.write(item,sys.stdout,"fastq")
+            except:
+                SeqIO.write(item, sys.stdout, "fasta")
 
 def main():
 
